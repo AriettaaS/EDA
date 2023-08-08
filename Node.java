d89dbe3bc637a0210c88677c4bfe8da124d98bd8
AriettaaS <93552006+AriettaaS@users.noreply.github.com>
2023-08-07 19:33:58 -0500

    Se añade las otras clases

--- Node.java
+++ Node.java
@@ -1,57 +1,31 @@
+package Clases.Listas;
 
+public class Node<E> {
+	private E data;
+	private Node<E> next;
+	
+	public Node(E data, Node<E> next) {
+		this.data = data;
+		this.next = next;
+	}
+	public Node(E data) {
+		this(data, null);
+	}
+	public E getData() {
+		return this.data;
+	}
+	public void setData(E data) {
+		this.data = data;
+	}
+	public Node<E> getNext() {
+		return this.next;
+	}
+	public void setNext(Node<E> next) {
+		this.next = next;
+	}
 
-public class Node<T extends Comparable<T>, V> {
-    private T key;
-    private V value;
-    private Node<T, V> left;
-    private Node<T, V> right;
-    private int height;
-
-    public Node(T key, V value) {
-        this.key = key;
-        this.value = value;
-        this.height = 1;
-    }
-
-    // Getters and setters
-    public T getKey() {
-        return key;
-    }
-
-    public V getValue() {
-        return value;
-    }
-
-    public Node<T, V> getLeft() {
-        return left;
-    }
-
-    public void setLeft(Node<T, V> left) {
-        this.left = left;
-    }
-
-    public Node<T, V> getRight() {
-        return right;
-    }
-
-    public void setRight(Node<T, V> right) {
-        this.right = right;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public void setKey(T key2) {
-        this.key = key2;
-    }
-
-    public void setValue(V value2) {
-        this.value = value2;
-    }
+	@Override
+	public String toString() {
+		return this.data.toString();
+	}
 }
-
